5d1c4934ce3f719fd14894950ab21122a9a5e6ea
Rupam Debnath <97718147+Rupam51015@users.noreply.github.com>
2026-09-01 11:03:21 +0000

    added brackets

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,8 +3,9 @@ def call(String credId, String imageName){
         credentialsId: "${credId}",
         passwordVariable: "dockerHubPass",
         usernameVariable: "dockerHubUser"
-    )])
+    )]) {
     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
     sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
     sh "docker push ${env.dockerHubUser}/${imageName}:latest"
+    }
 }

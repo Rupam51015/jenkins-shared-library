f0004c09c19f622a88e55062906c8b7cf0d729da
Rupam Debnath <97718147+Rupam51015@users.noreply.github.com>
2026-09-02 20:07:45 +0530

    Refactor docker_build.groovy for consistency

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,8 +1,8 @@
-def call(Map config = [:]){
-    def imageTag = config.imageTag ?: 'latest';
+def call(Map config = [:]) {
+    def imageTag  = config.imageTag ?: 'latest';
     def imageName = config.imageName ?: error("Image Name is required");
-    def fileName = config.fileName ?: 'Dockerfile';
-    def context = config.context ?: '.'
+    def fileName  = config.fileName ?: 'Dockerfile';
+    def context   = config.context ?: '.'
 
     echo "${imageName}:${imageTag} Image is built"
     sh "docker build --no-cache -f ${fileName} -t ${imageName}:${imageTag} ${context}"
